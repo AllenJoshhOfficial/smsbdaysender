05aadb1db0a49a28bd3a9c923dfe1fa173bc9ef1
Joshua Allen <161228650+AllenJoshhOfficial@users.noreply.github.com>
2024-07-29 21:55:37 +0530

    Update from selenium import webdriver.py

--- from selenium import webdriver.py
+++ from selenium import webdriver.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-# Create a ChromeOptions object
+
 chrome_options = Options()
 
-# Add options to the ChromeOptions object
+
 chrome_options.add_argument('--disable-extensions')
 
-# Pass the ChromeOptions object to the Chrome driver constructor
+
 driver = webdriver.Chrome(options=chrome_options)
